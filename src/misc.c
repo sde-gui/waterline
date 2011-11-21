52053a465d24a06988d2a6786f3fb3d33a8a77d5
Vadim Ushakov <igeekless@gmail.com>
2011-11-21 06:38:58 +0000

    Fix fb_button label alignment
    git-svn-id: http://lxpanelx.googlecode.com/svn/trunk@196 36a17009-62c4-608e-612b-5d11f7c201f1

--- src/misc.c
+++ src/misc.c
@@ -355,7 +355,7 @@ GtkWidget * fb_button_new_from_file_with_label(
         GtkWidget * lbl = gtk_label_new("");
         panel_draw_label_text(panel, lbl, label, FALSE, TRUE);
         gtk_misc_set_padding(GTK_MISC(lbl), 2, 0);
-        gtk_box_pack_end(GTK_BOX(inner), lbl, FALSE, FALSE, 0);
+        gtk_box_pack_start(GTK_BOX(inner), lbl, FALSE, FALSE, 0);
     }
 
     gtk_misc_set_alignment(GTK_MISC(image), 0.5, 0.5);

1267d775652f53da321cc9659fc4b2a5b5283a54
Vadim Ushakov <igeekless@gmail.com>
2012-07-27 09:03:08 +0000

    remove SuxPanel copyrights as there is no SuxPanel code in misc.c, in fact.
    git-svn-id: http://lxpanelx.googlecode.com/svn/trunk@502 36a17009-62c4-608e-612b-5d11f7c201f1

--- src/misc.c
+++ src/misc.c
@@ -17,17 +17,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/*
- * SuxPanel version 0.1
- * Copyright (c) 2003 Leandro Pereira <leandro@linuxmag.com.br>
- *
- * This program may be distributed under the terms of GNU General
- * Public License version 2. You should have received a copy of the
- * license with this program; if not, please consult http://www.fsf.org/.
- *
- * This program comes with no warranty. Use at your own risk.
- *
- */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>

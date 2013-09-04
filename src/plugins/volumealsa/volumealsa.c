af35ee7443ba83289edd2b328f9b63f2e935197c
Vadim Ushakov <igeekless@gmail.com>
2013-09-04 20:03:38 +0800

    volumealsa: Make the plugin description translatable

--- src/plugins/volumealsa/volumealsa.c
+++ src/plugins/volumealsa/volumealsa.c
@@ -610,7 +610,7 @@ PluginClass volumealsa_plugin_class = {
     type : "volumealsa",
     name : N_("Volume Control (ALSA)"),
     version: "1.0",
-    description : "Display and control volume for ALSA",
+    description : N_("Display and control volume for ALSA"),
 
     constructor : volumealsa_constructor,
     destructor  : volumealsa_destructor,

91b5552a36f3ab3c18d3181a65f61586e5edcf74
Vadim Ushakov <igeekless@gmail.com>
2013-09-05 18:11:55 +0800

    move translate_app_exec_to_command_line() to sde-utils

--- src/misc.c
+++ src/misc.c
@@ -37,6 +37,7 @@
 #include <stdarg.h>
 #include <pwd.h>
 #include <grp.h>
+#include <sde-utils.h>
 
 #include <waterline/global.h>
 #include <waterline/misc.h>
@@ -127,161 +128,6 @@ char* translate_exec_to_cmd(const char * exec, const char * icon,
 
 /********************************************************************/
 
-/*
- * Taken from pcmanfm:
- * Parse Exec command line of app desktop file, and translate
- * it into a real command which can be passed to g_spawn_command_line_async().
- * file_list is a null-terminated file list containing full
- * paths of the files passed to app.
- * returned char* should be freed when no longer needed.
- */
-static char* translate_app_exec_to_command_line( const char* pexec,
-                                                 GList* file_list )
-{
-    char* file;
-    GList* l;
-    gchar *tmp;
-    GString* cmd = g_string_new("");
-    gboolean add_files = FALSE;
-
-    for( ; *pexec; ++pexec )
-    {
-        if( *pexec == '%' )
-        {
-            ++pexec;
-            switch( *pexec )
-            {
-            case 'U':
-                for( l = file_list; l; l = l->next )
-                {
-                    tmp = g_filename_to_uri( (char*)l->data, NULL, NULL );
-                    file = g_shell_quote( tmp );
-                    g_free( tmp );
-                    g_string_append( cmd, file );
-                    if (l->next)
-                        g_string_append_c( cmd, ' ' );
-                    g_free( file );
-                }
-                add_files = TRUE;
-                break;
-            case 'u':
-                if( file_list && file_list->data )
-                {
-                    file = (char*)file_list->data;
-                    tmp = g_filename_to_uri( file, NULL, NULL );
-                    file = g_shell_quote( tmp );
-                    g_free( tmp );
-                    g_string_append( cmd, file );
-                    g_free( file );
-                    add_files = TRUE;
-                }
-                break;
-            case 'F':
-            case 'N':
-                for( l = file_list; l; l = l->next )
-                {
-                    file = (char*)l->data;
-                    tmp = g_shell_quote( file );
-                    g_string_append( cmd, tmp );
-                    if (l->next)
-                        g_string_append_c( cmd, ' ' );
-                    g_free( tmp );
-                }
-                add_files = TRUE;
-                break;
-            case 'f':
-            case 'n':
-                if( file_list && file_list->data )
-                {
-                    file = (char*)file_list->data;
-                    tmp = g_shell_quote( file );
-                    g_string_append( cmd, tmp );
-                    g_free( tmp );
-                    add_files = TRUE;
-                }
-                break;
-            case 'D':
-                for( l = file_list; l; l = l->next )
-                {
-                    tmp = g_path_get_dirname( (char*)l->data );
-                    file = g_shell_quote( tmp );
-                    g_free( tmp );
-                    g_string_append( cmd, file );
-                    if (l->next)
-                        g_string_append_c( cmd, ' ' );
-                    g_free( file );
-                }
-                add_files = TRUE;
-                break;
-            case 'd':
-                if( file_list && file_list->data )
-                {
-                    tmp = g_path_get_dirname( (char*)file_list->data );
-                    file = g_shell_quote( tmp );
-                    g_free( tmp );
-                    g_string_append( cmd, file );
-                    g_free( tmp );
-                    add_files = TRUE;
-                }
-                break;
-            case 'c':
-                #if 0
-                g_string_append( cmd, vfs_app_desktop_get_disp_name( app ) );
-                #endif
-                break;
-            case 'i':
-                /* Add icon name */
-                #if 0
-                if( vfs_app_desktop_get_icon_name( app ) )
-                {
-                    g_string_append( cmd, "--icon " );
-                    g_string_append( cmd, vfs_app_desktop_get_icon_name( app ) );
-                }
-                #endif
-                break;
-            case 'k':
-                /* Location of the desktop file */
-                break;
-            case 'v':
-                /* Device name */
-                break;
-            case '%':
-                g_string_append_c ( cmd, '%' );
-                break;
-            case '\0':
-                goto _finish;
-                break;
-            }
-        }
-        else  /* not % escaped part */
-        {
-            g_string_append_c ( cmd, *pexec );
-        }
-    }
-_finish:
-    if( ! add_files )
-    {
-        for( l = file_list; l; l = l->next )
-        {
-            g_string_append_c( cmd, ' ' );
-            file = (char*)l->data;
-            tmp = g_shell_quote( file );
-            g_string_append( cmd, tmp );
-            g_free( tmp );
-        }
-    }
-
-    gchar * result = g_string_free( cmd, FALSE );
-
-    int len = strlen(result);
-    while (len && result[len - 1] == ' ')
-    {
-        result[--len] = 0;
-    }
-
-    return result;
-}
-
 gboolean wtl_launch(const char* command, GList* files)
 {
     if (!command)
@@ -308,7 +154,7 @@ gboolean wtl_launch_app(const char* exec, GList* files, gboolean in_terminal)
     char* cmd;
     if( ! exec )
         return FALSE;
-    cmd = translate_app_exec_to_command_line(exec, files);
+    cmd = su_translate_app_exec_to_command_line(exec, files);
     if( in_terminal )
     {
         char* term_cmd;

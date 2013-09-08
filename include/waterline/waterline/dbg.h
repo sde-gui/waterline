a9e63bd4f96f7d9c5ea10b21d6cd3c58286788ce
Vadim Ushakov <igeekless@gmail.com>
2013-09-08 19:58:24 +0800

    Move logging facilities to sde-utils

--- include/waterline/waterline/dbg.h
+++ include/waterline/waterline/dbg.h
@@ -19,53 +19,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+#include <sde-utils.h>
+
 #define DBG2(fmt, args...) fprintf(stderr, "%s:%s:%-5d: " fmt, __FILE__,  __FUNCTION__, __LINE__, ## args)
 #define ENTER2          do { fprintf(stderr, "%s:%s:%-5d: ENTER\n",  __FILE__,__FUNCTION__, __LINE__); } while(0)
 #define RET2(args...)   do { fprintf(stderr, "%s:%s:%-5d: RETURN\n",  __FILE__,__FUNCTION__, __LINE__);\
 return args; } while(0)
-
-
-typedef enum _SU_LOG_LEVEL {
-    SU_LOG_NONE,
-    SU_LOG_ERROR,
-    SU_LOG_WARNING,
-    SU_LOG_INFO,
-    SU_LOG_DEBUG,
-    SU_LOG_DEBUG_SPAM,
-    SU_LOG_DEBUG_2 = SU_LOG_DEBUG_SPAM,
-    SU_LOG_ALL
-} SU_LOG_LEVEL;
-
-void su_log_message_va(SU_LOG_LEVEL level, const char * format, va_list ap);
-void su_log_message   (SU_LOG_LEVEL level, const char * format, ...);
-void su_log_error     (const char * format, ...);
-void su_log_warning   (const char * format, ...);
-void su_log_info      (const char * format, ...);
-void su_log_debug     (const char * format, ...);
-void su_log_debug2    (const char * format, ...);
-
-extern int su_log_level;
-
-#define SU_LOG_DEBUG(fmt, args...)  do { if (su_log_level >= SU_LOG_DEBUG  ) su_log_debug(fmt, ## args); } while(0)
-#define SU_LOG_DEBUG2(fmt, args...) do { if (su_log_level >= SU_LOG_DEBUG_2) su_log_debug2(fmt, ## args); } while(0)
-
-
-void su_print_error_message(const char *string, ...);
 
 #ifdef DEBUG
 
 #define ENTER          do { log_message(LOG_DBG, "%s:%s:%-5d: ENTER\n",  __FILE__,__FUNCTION__, __LINE__); } while(0)
 #define RET(args...)   do { log_message(LOG_DBG, "%s:%s:%-5d: RETURN\n", __FILE__, __FUNCTION__, __LINE__);\
 return args; } while(0)
-//#define DBG(fmt, args...) log_message(LOG_DBG, "%s:%s:%-5d: " fmt,  __FILE__,__FUNCTION__, __LINE__, ## args)
-//#define LOG(level, fmt, args...) log_message(level, fmt, ## args)
 
 #else
 
 #define ENTER         do {  } while(0)
 #define RET(args...)   return args
-//#define DBG(fmt, args...)   do {  } while(0)
-//#define LOG(level, fmt, args...) do { if (level <= log_level) log_message(level, fmt, ## args); } while(0)
 
 #endif
 

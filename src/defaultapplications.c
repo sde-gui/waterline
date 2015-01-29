f831970a3fc24b28ceb2876a34123184713f7ac2
Vadim Ushakov <igeekless@gmail.com>
2015-01-29 16:22:17 +0700

    Add support for DE hints in default application lists.

--- src/defaultapplications.c
+++ src/defaultapplications.c
@@ -20,6 +20,7 @@
 #include <config.h>
 #endif
 
+#include <fnmatch.h>
 #include <sde-utils.h>
 
 #include <waterline/defaultapplications.h>
@@ -66,52 +67,80 @@ const char * wtl_get_default_application(char * type)
             return result;
     }
 
-    if (!default_applications)
-        default_applications = g_hash_table_new(NULL, g_str_equal);
-    if (!default_applications)
-        return NULL;
+    {
+        if (!default_applications)
+            default_applications = g_hash_table_new(NULL, g_str_equal);
+        if (!default_applications)
+            return NULL;
 
-    gchar * result = g_hash_table_lookup(default_applications, type);
-    if (result)
-        return result;
+        gchar * result = g_hash_table_lookup(default_applications, type);
+        if (result)
+            return result;
+    }
+
+    const char * de_name = get_de_name();
+
+again: ;
 
     gchar * file_name = g_build_filename("applications", type, NULL);
     if (!file_name)
         return NULL;
 
     gchar ** lines = read_list_from_config(file_name);
+    g_free(file_name);
     if (!lines)
         return NULL;
 
     gchar ** l;
     for (l = lines; *l; l++)
     {
+        gchar * line = *l;
+
+        while(g_ascii_isspace(*line)) /* skip leading whitespaces */
+            line++;
+
+        if (*line == '#' || *line == 0) /* ignore comments and empty lines */
+            continue;
+
+        if (*line == '[') { /* DE specifier */
+            gchar * close_bracket_position = g_strstr_len(line, -1, "]");
+            if (!close_bracket_position) /* invalid syntax, ignore line */
+                continue;
+            gchar * pattern = line + 1;
+            *close_bracket_position = 0;
+            line = close_bracket_position + 1;
+            if (!su_str_empty(de_name)) {
+                if (fnmatch(pattern, de_name, 0) != 0)
+                    continue;
+            }
+        }
+        else {
+            if (!su_str_empty(de_name)) /* skip line without a DE specifier when running the first (DE-matching) pass */
+                continue;
+        }
+
         /* Split into words and check if command present in the system. */
-        gchar ** words = g_strsplit_set(*l, "\t ", 0);
+        gchar ** words = g_strsplit_set(line, "\t ", 0);
         gchar ** w;
         for (w = words; *w; w++)
         {
             gchar * word = *w;
             if (!word) /* empty line */
                 break;
-amp_removed:
-            if (*word == '#') /* comment, ignore that line */
-                break;
-            if (*word == 0) /* empty word just means two or more adjacent separators */
-                continue;
-            if (*word == '&') /* ignore '&' */
-            {
+
+            while (*word == '&') /* skip '&' */
                 word++;
-                goto amp_removed;
-            }
+
+            if (*word == 0) /* empty word due to redundant whitespaces, skip it*/
+                continue;
 
             gchar * program_path = g_find_program_in_path(word);
             g_free(program_path);
-            g_strfreev(words);
 
             if (program_path)
             {
-                result = g_strdup(*l);
+                g_strfreev(words);
+                gchar * result = g_strdup(line);
                 g_strfreev(lines);
                 g_hash_table_insert(default_applications, type, result);
                 return result;
@@ -120,9 +149,16 @@ amp_removed:
             break;
         }// for words
 
+        g_strfreev(words);
+
     }// for lines
 
     g_strfreev(lines);
+
+    if (!su_str_empty(de_name)) {
+        de_name = NULL;
+        goto again;
+    }
 
     return NULL;
 }

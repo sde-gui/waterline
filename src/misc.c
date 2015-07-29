5c2aff479dac4ef487c156498a4f0d4f9a7260ae
Vadim Ushakov <igeekless@gmail.com>
2015-07-29 16:50:26 +0700

    code style fixes in misc.c

--- src/misc.c
+++ src/misc.c
@@ -321,21 +321,21 @@ static char ftypelet (mode_t bits)
 static void strmode (mode_t mode, char *str)
 {
   str[0] = ftypelet (mode);
-  str[1] = mode & S_IRUSR ? 'r' : '-';
-  str[2] = mode & S_IWUSR ? 'w' : '-';
-  str[3] = (mode & S_ISUID
-            ? (mode & S_IXUSR ? 's' : 'S')
-            : (mode & S_IXUSR ? 'x' : '-'));
-  str[4] = mode & S_IRGRP ? 'r' : '-';
-  str[5] = mode & S_IWGRP ? 'w' : '-';
-  str[6] = (mode & S_ISGID
-            ? (mode & S_IXGRP ? 's' : 'S')
-            : (mode & S_IXGRP ? 'x' : '-'));
-  str[7] = mode & S_IROTH ? 'r' : '-';
-  str[8] = mode & S_IWOTH ? 'w' : '-';
-  str[9] = (mode & S_ISVTX
-            ? (mode & S_IXOTH ? 't' : 'T')
-            : (mode & S_IXOTH ? 'x' : '-'));
+  str[1] = (mode & S_IRUSR) ? 'r' : '-';
+  str[2] = (mode & S_IWUSR) ? 'w' : '-';
+  str[3] = ((mode & S_ISUID)
+            ? ((mode & S_IXUSR) ? 's' : 'S')
+            : ((mode & S_IXUSR) ? 'x' : '-'));
+  str[4] = (mode & S_IRGRP) ? 'r' : '-';
+  str[5] = (mode & S_IWGRP) ? 'w' : '-';
+  str[6] = ((mode & S_ISGID)
+            ? ((mode & S_IXGRP) ? 's' : 'S')
+            : ((mode & S_IXGRP) ? 'x' : '-'));
+  str[7] = (mode & S_IROTH) ? 'r' : '-';
+  str[8] = (mode & S_IWOTH) ? 'w' : '-';
+  str[9] = ((mode & S_ISVTX)
+            ? ((mode & S_IXOTH) ? 't' : 'T')
+            : ((mode & S_IXOTH) ? 'x' : '-'));
   str[10] = ' ';
   str[11] = '\0';
 }
